c2c0d92a9134dd73454caddc34d961cb1b43fb23
Rebecca Liu <qu6qu6g@gmail.com>
2024-09-24 00:16:27 -0400

    added validation for user input

--- src/Textarea.jsx
+++ src/Textarea.jsx
@@ -2,17 +2,31 @@ import { useState } from "react";
 
 const Textarea = () => {
   const [text, setText] = useState("");
+  const [warning, setWarning] = useState("");
+
+  const handleChange = (e) => {
+    let newText = e.target.value;
+    if (newText.includes("<script>")) {
+      setWarning("No script tag allowed!");
+      setTimeout(() => setWarning(""), 1500);
+      newText = newText.replace("<script>", "");
+    }
+    setText(newText);
+  };
+
   return (
     <>
-      <textarea
-        value={text}
-        onChange={(e) => {
-          setText(e.target.value);
-        }}
-        className="textarea"
-        placeholder="Enter your text"
-        spellCheck="false"
-      />
+      <div className="textarea">
+        <textarea
+          value={text}
+          onChange={handleChange}
+          className="textarea"
+          placeholder="Enter your text"
+          spellCheck="false"
+        />
+
+        <p className="warning">{warning}</p>
+      </div>
     </>
   );
 };
